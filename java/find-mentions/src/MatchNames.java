3988c2162705767a4e812df88e34f012b2ce5c27
Michael Axiak <mike@axiak.net>
2011-06-25 16:22:36 -0400

    Updated to use s3.

--- java/find-mentions/src/MatchNames.java
+++ java/find-mentions/src/MatchNames.java
@@ -9,39 +9,42 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
+import java.util.Arrays;
 
 
 public class MatchNames extends Configured implements Tool {
   public static final String INPUT_PATH = "/datasets/ngrams/";
+  public static final String S3_PATH = "s3://datasets.elasticmapreduce/ngrams/books/20090715/eng-us-all/%sgram/data";
 	public static final String OUTPUT_PATH = "/mnt/team6/bpopp/out";
-	
+
 	@Override
 	public int run(String[] arg0) throws Exception {
-			
+
 		Configuration conf = getConf();
-	
+
 		FileSystem hdfs = FileSystem.get(conf);
 		hdfs.delete(new Path(OUTPUT_PATH), true);
-		
+
 		Job job = new Job(conf, "match-names");
 		job.setJarByClass(MatchNames.class);
 
 		job.setMapperClass(MatchNamesMapper.class);
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(Text.class);
-		
+
 		FileOutputFormat.setOutputPath(job, new Path(OUTPUT_PATH));
-		FileInputFormat.addInputPath(job, new Path(INPUT_PATH + "1gram"));
+    for (String i : Arrays.asList("2", "3")) {
+		  FileInputFormat.addInputPath(job, new Path(String.format(S3_PATH, i)));
+    }
 		FileInputFormat.addInputPath(job, new Path(INPUT_PATH + "2gram"));
-		
-		return job.waitForCompletion(true) ? 0 : 1;		
+		return job.waitForCompletion(true) ? 0 : 1;
 	}
 	
 	
     public static void main(String[] args) throws Exception {
-        // Let ToolRunner handle generic command-line options 
-        int res = ToolRunner.run(new Configuration(), new MatchNames(), args);
+      // Let ToolRunner handle generic command-line options
+      int res = ToolRunner.run(new Configuration(), new MatchNames(), args);
         
-        System.exit(res);
-      }
+      System.exit(res);
+    }
 }
